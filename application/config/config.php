d5725323902a7bddc83b930626dbe072bff774e9
IAMR7 <reza.febri72@gmail.com>
2022-06-20 11:09:39 +0700

    fixing checked jenis kelamin

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/belajar-crud/';
+$config['base_url'] = 'https://tugas-hosting-crud-ci-3.000webhostapp.com/';
 
 /*
 |--------------------------------------------------------------------------
